e74080298341448d861a09604ccf142b16927ba8
Roman <roman.atachiants@gmail.com>
2019-02-18 22:43:44 +0800

    randomised

--- spiral.go
+++ spiral.go
@@ -2,23 +2,47 @@ package spiral
 
 import (
 	"math"
+	"math/rand"
 )
 
-var epsilon = math.Nextafter(1, 2) - 1
+const twoPi = 2 * math.Pi
 
 // https://arxiv.org/ftp/arxiv/papers/0908/0908.0892.pdf
 func galaxy(phi, scale, b, n float64) float64 {
 	return scale / (math.Log(b * math.Tan(phi/(2*n))))
 }
 
-func gudermannian(x float64) float64 {
-	return 2 * math.Pow(math.Tan(math.Pow(math.E, x)), -1)
+type Galaxy struct {
 }
 
-func lobachevsky(x float64) float64 {
-	return 2 * math.Pow(math.Tan(math.Pow(math.E, -x)), -1)
+func (g Galaxy) Generate(random *rand.Rand) {
+
 }
 
-func logspiral(r, k, phi float64) float64 {
-	return r * math.Pow(math.E, k*phi)
+// Arm represents a single arm for the spiral galaxy
+type Arm struct {
+	Bulge float64 // Greater buldge (B) results in greater arm sweep and smaller bar/bulge
+	Tight float64 // Tight controls the tightness (N)
+	Pitch float64 // Pitch of the arm
+	Noise float64 // Noise controls the randomness factor applied
+}
+
+// Generate generates a single spiral galaxy arm. This uses the function described in
+// the paper https://arxiv.org/ftp/arxiv/papers/0908/0908.0892.pdf for realism.
+func (a Arm) Generate(random *rand.Rand, count int) (stars []Star) {
+	for phi := .0; phi < twoPi; phi += twoPi / float64(count) {
+		r := 1 / (math.Log(a.Bulge * math.Tan(phi/(2*a.Tight))))
+		rx := random.Float64() * a.Noise
+		ry := random.Float64() * a.Noise
+		stars = append(stars, Star{
+			X: r*math.Cos(phi+a.Pitch) + rx,
+			Y: r*math.Sin(phi+a.Pitch) + ry,
+		})
+	}
+	return
+}
+
+// Star represents a single star
+type Star struct {
+	X, Y float64
 }
--- spiral_test.go
+++ spiral_test.go
@@ -3,53 +3,48 @@ package spiral
 import (
 	"math"
 	"fmt"
+	"math/rand"
 	"testing"
 
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
 	"github.com/stretchr/testify/assert"
 )
 
+var random = rand.New(rand.NewSource(42))
+
+
 func TestBuilder(t *testing.T) {
 	series := []plotter.XYs{}
-	const arms = 6.0
+	const arms = 7.0
 	for i := .0; i < arms; i++{
 		t := i / arms * 2*math.Pi
-		println(t)
 		series = append(series, drawImage(t))
 	}
-	assert.Fail(t, "")
 	
 	fmt.Println("saving image")
 	saveImage(t, "output", series...)
 }
 
 func drawImage(t float64) (points plotter.XYs){
-	const r1, a1 = 0.05, 0
-	const r2, a2 = 0.85, 2*math.Pi
-	const N = 100.0
-	
-	for i := .0; i < N; i++ {
-		phi := i * ((a2 - a1) / N)
-		r := galaxy(phi, 1, .5, 4)
-		phi += t
-
-		x := r * math.Cos(phi)
-		y := r * math.Sin(phi)
-		points = append(points, plotter.XY{ X: x, Y: y})
+	arm := Arm{ Bulge: 0.55, Tight: 3.7, Pitch: t, Noise: .15}
+	for _, v := range arm.Generate(random, 100){
+		points = append(points, plotter.XY{ X: v.X, Y: v.Y})
 	}
 	return
 }
 
 func saveImage(t *testing.T, name string, series ...plotter.XYs){
+	plotutil.DefaultGlyphShapes = []draw.GlyphDrawer{ draw.CrossGlyph{}	}
 	p, _ := plot.New()
 	p.X.Label.Text = "X"
 	p.Y.Label.Text = "Y"
 	for _, s := range series{
-		plotutil.AddLinePoints(p, "", s)
+		plotutil.AddScatters(p, s)
 	}
-	err := p.Save(4*vg.Inch, 4*vg.Inch, name +".png"); 
+	err := p.Save(5*vg.Inch, 5*vg.Inch, name +".png"); 
 	assert.NoError(t, err)
 }
