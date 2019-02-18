8fb9790bca95d26a0dc4c93350df6be46ec68105
Roman <roman.atachiants@gmail.com>
2019-02-18 19:25:14 +0800

    fixed

--- spiral_test.go
+++ spiral_test.go
@@ -14,33 +14,28 @@ import (
 
 func TestBuilder(t *testing.T) {
 	series := []plotter.XYs{}
-	for i := .0; i < 6; i++{
-		series = append(series, drawImage(i))
+	const arms = 6.0
+	for i := .0; i < arms; i++{
+		t := i / arms * 2*math.Pi
+		println(t)
+		series = append(series, drawImage(t))
 	}
+	assert.Fail(t, "")
 	
 	fmt.Println("saving image")
 	saveImage(t, "output", series...)
 }
 
 func drawImage(t float64) (points plotter.XYs){
-	const r1, a1 = 0.05, -math.Pi 
-	const r2, a2 = 0.85, +math.Pi
+	const r1, a1 = 0.05, 0
+	const r2, a2 = 0.85, 2*math.Pi
 	const N = 100.0
 	
 	for i := .0; i < N; i++ {
-		r := i * ((r2 - r1) / N)
-
-		//phi := i * ((a2 - a1) / N)
-		//phi := math.Pow(math.E, -math.Pow(epsilon*r, 2))
-		//phi := math.Pow(r, 2) * math.Log(r)
-		//phi := math.Pow(r, 5)
-		//phi := lobachevsky(r)
-		phi := gudermannian(r)
+		phi := i * ((a2 - a1) / N)
+		r := galaxy(phi, 1, .5, 4)
 		phi += t
-	
-		//r = logspiral(r, .15, phi)
-		r = galaxy(r, 1, 5, 3.5)
-		
+
 		x := r * math.Cos(phi)
 		y := r * math.Sin(phi)
 		points = append(points, plotter.XY{ X: x, Y: y})
